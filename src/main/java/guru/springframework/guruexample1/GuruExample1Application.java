7448394282712ac6b870dfb817f3f39e0a1f54ff
ismaelchaquir <chaquirbemat@gmail.com>
2022-04-22 18:29:41 +0200

    first commit

---
+++ src/main/java/guru/springframework/guruexample1/GuruExample1Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.guruexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/guruexample1/GuruExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.guruexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
